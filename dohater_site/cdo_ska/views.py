79248a2192bcec5db3d28bc0fb8b96a18931707c
Joe <pavlovks2004@yandex.ru>
2023-02-26 18:31:57 +0300

    sidebar prototype ended

--- dohater_site/cdo_ska/views.py
+++ dohater_site/cdo_ska/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'cdo_ska/index.html')
+
+def home(request):
+    return render(request, 'cdo_ska/home.html')
